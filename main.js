aec8128fac349189000e7523536e2e93cba854e2
Takahiro Abe <hirotaka081630@gmail.com>
2021-10-17 20:35:22 +0900

    feat: .gitignoreの作成

--- main.js
+++ main.js
@@ -9,18 +9,34 @@ class Keybord {
 
 const itemBox = [
   new Keybord(1, 'keychron K2', "It's a very cool keybord!!", 'https://source.unsplash.com/KYw1eUx1J7Y'),
-  new Keybord(2, 'keychron K3', "It's a very cool pc!!", 'https://source.unsplash.com/TxXuh_hAFd8'),
-  new Keybord(3, 'keychron K4', '3', 'https://source.unsplash.com/KYw1eUx1J7Y'),
-  new Keybord(4, 'keychron K5', '4', 'https://source.unsplash.com/TxXuh_hAFd8'),
+  new Keybord(2, 'A cool computer', "It's a very cool pc!!", 'https://source.unsplash.com/TxXuh_hAFd8'),
+  new Keybord(3, 'Office PC', 'black pc is cool', 'https://source.unsplash.com/2nCeVeFWA_c'),
+  new Keybord(4, 'ubuntu', 'I use ubuntu for my main OS', 'https://source.unsplash.com/xbEVM6oJ1Fs'),
+  new Keybord(5, 'linux', 'I want to master how to use linux', 'https://source.unsplash.com/Tjbk79TARiE'),
+  new Keybord(6, 'nature', 'Nature makes me relax', 'https://source.unsplash.com/NRQV-hBF10M'),
+  new Keybord(7, 'nature', 'Nature makes me relax', 'https://source.unsplash.com/M6XC789HLe8'),
+  new Keybord(8, 'forest', 'Forest makes me happy', 'https://source.unsplash.com/sp-p7uuT0tw'),
+  new Keybord(9, 'sea', 'I want to go to sea and swim in', 'https://source.unsplash.com/KMn4VEeEPR8'),
+  new Keybord(10, 'keychron K2', "It's a very cool keybord!!", 'https://source.unsplash.com/KYw1eUx1J7Y'),
+  new Keybord(11, 'A cool computer', "It's a very cool pc!!", 'https://source.unsplash.com/TxXuh_hAFd8'),
+  new Keybord(12, 'Office PC', 'black pc is cool', 'https://source.unsplash.com/2nCeVeFWA_c'),
+  new Keybord(13, 'ubuntu', 'I use ubuntu for my main OS', 'https://source.unsplash.com/xbEVM6oJ1Fs'),
+  new Keybord(14, 'linux', 'I want to master how to use linux', 'https://source.unsplash.com/Tjbk79TARiE'),
+  new Keybord(15, 'nature', 'Nature makes me relax', 'https://source.unsplash.com/NRQV-hBF10M'),
+  new Keybord(16, 'nature', 'Nature makes me relax', 'https://source.unsplash.com/M6XC789HLe8'),
+  new Keybord(17, 'forest', 'Forest makes me happy', 'https://source.unsplash.com/sp-p7uuT0tw'),
+  new Keybord(18, 'sea', 'I want to go to sea and swim in', 'https://source.unsplash.com/KMn4VEeEPR8'),
 ];
 
 let sliderShow = document.createElement('div');
 let main = document.createElement('div');
 let extra = document.createElement('div');
+const warning = document.createElement('div');
 
 sliderShow.classList.add('d-flex', 'flex-nowrap', 'overflow-hiddens');
 main.classList.add('main__img');
 extra.classList.add('extra__img');
+warning.classList.add('warning');
 
 // main.append(itemBox[0]);
 main.style.backgroundImage = 'url(' + itemBox[0].imgUrl + ')';
@@ -39,6 +55,7 @@ comment.innerHTML = itemBox[0].comment;
 
 const target = document.getElementById('target');
 target.append(sliderShow);
+target.append(warning);
 
 function slideJump(steps, animationType) {
   let index = parseInt(main.getAttribute('data-index'));
@@ -59,11 +76,9 @@ function slideJump(steps, animationType) {
 
 function animateMain(currentElement, nextElement, animationType) {
   main.innerHTML = '';
-  // main.append(nextElement);
   main.style.backgroundImage = 'url(' + nextElement.imgUrl + ')';
 
   extra.innerHTML = '';
-  // extra.append(currentElement);
   extra.style.backgroundImage = 'url(' + currentElement.imgUrl + ')';
 
   number.innerHTML = nextElement.id;
@@ -86,13 +101,6 @@ function animateMain(currentElement, nextElement, animationType) {
 
 const leftBtn = document.getElementById('1');
 const rightBtn = document.getElementById('2');
-
-// leftBtn.addEventListener('click', function () {
-//   slideJump(-1, 'left');
-// });
-// rightBtn.addEventListener('click', function () {
-//   slideJump(+1, 'right');
-// });
 
 const numberBtns = document.querySelectorAll('.btn-number');
 const meter = document.getElementById('meter');
@@ -126,6 +134,7 @@ function isNextElement(index) {
 clear.addEventListener('click', function () {
   changeDisabledBtn();
   meter.innerHTML = '';
+  warning.innerHTML = '';
 });
 
 submit.addEventListener('click', function () {
@@ -133,11 +142,35 @@ submit.addEventListener('click', function () {
   const currentIndex = parseInt(main.getAttribute('data-index'));
   const nextIndex = meter.innerHTML;
 
-  isNextElement(nextIndex);
+  if (nextIndex > itemBox.length) {
+    warning.innerHTML = `現在の登録数は${itemBox.length}です`;
+    console.log(`現在の登録数は${itemBox.length}です`);
+    return;
+  }
 
   const currentElement = itemBox[currentIndex - 1];
   const nextElement = itemBox[nextIndex - 1];
 
   main.setAttribute('data-index', nextIndex.toString());
-  animateMain(currentElement, nextElement, 'right');
+  animateMain(currentElement, nextElement, defineDirection(parseInt(currentIndex), parseInt(nextIndex)));
 });
+
+function defineDirection(currentIndex, nextIndex) {
+  let rc;
+  let lc;
+  if (currentIndex > nextIndex) {
+    rc = itemBox.length + nextIndex - currentIndex;
+    lc = currentIndex - nextIndex;
+  } else {
+    rc = nextIndex - currentIndex;
+    lc = currentIndex + itemBox.length - nextIndex;
+  }
+  if (rc > lc) {
+    console.log(`rc: ${rc}, lc: ${lc}`);
+    console.log('left');
+  } else {
+    console.log(`rc: ${rc}, lc: ${lc}`);
+    console.log('right');
+  }
+  return rc > lc ? 'left' : 'right';
+}
